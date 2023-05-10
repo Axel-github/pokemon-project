a5ba93df04372de9485104c4c3bafe5267b1ca5a
Axel-github <ouskornikov@mail.ru>
2023-05-10 22:29:23 +0300

    Заглушка на изображения для карточек

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,25 +1,26 @@
 import React, { useState } from "react";
 import "./layout.css";
+import
 
 
-function Layout(props) {
+  function Layout(props) {
 
 
-  const [bgColor, setBgColor] = useState("#00bfff "); // начальный цвет фона
+    const [bgColor, setBgColor] = useState("#00bfff "); // начальный цвет фона
 
-  const changeBgColor = () => {
-    // генерируем случайный цвет в формате HEX
-    const newColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-    setBgColor(newColor); // устанавливаем новый цвет фона
-  };
+    const changeBgColor = () => {
+      // генерируем случайный цвет в формате HEX
+      const newColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+      setBgColor(newColor); // устанавливаем новый цвет фона
+    };
 
 
-  return (
-    <div className="layout" style={{ backgroundColor: bgColor }}>
-      {/* <button className="btn layout_change_background_button" onClick={changeBgColor}>Сменить фон</button> */}
-      {props.children}
-    </div>
-  );
-}
+    return (
+      <div className="layout" style={{ backgroundColor: bgColor }}>
+        {/* <button className="btn layout_change_background_button" onClick={changeBgColor}>Сменить фон</button> */}
+        {props.children}
+      </div>
+    );
+  }
 
 export default Layout;
