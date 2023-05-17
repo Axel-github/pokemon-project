13082ddd409a04aaf805a896470f8507ee8f2bf3
Axel-github <ouskornikov@mail.ru>
2023-05-17 14:00:28 +0300

    +рамка для имени

--- src/components/card/PokemonCard.js
+++ src/components/card/PokemonCard.js
@@ -1,30 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./card.css";
+import getPokemonSprite from "../../helpers/getPokemonSprite";
 
 
 const PokemonCard = ({ pokemon, setPokemon, setOpen }) => {
 
   const [name] = useState(pokemon.name);
   //const [weight] = useState(pokemon.weight);
-  const [spriteUrl] = useState(pokemon.spriteUrl);
+  const [spriteUrl, setSpriteUrl] = useState(getPokemonSprite(0, 'static'));
   // const [isOpen, setIsOpen] = useState(false);
 
   const handleCloseModal = () => {
     setOpen(true);
     setPokemon(pokemon);
   };
+  useEffect(() => {
+    setSpriteUrl(pokemon.spriteUrl);
+  }, [pokemon]);
 
   return (
-    
-      <div className="card" onClick={() => handleCloseModal()}>
-        <div className="card-body">
-          <img className="card-body-image" src={spriteUrl} alt='pokemon' />
-        </div>
-        <div className="card-footer">
-          <span className="card-footer-title">{name}</span>
-        </div>
+
+    <div className="card" onClick={() => handleCloseModal()}>
+      <div className="card-body">
+        <img className="card-body-image" src={spriteUrl} alt='pokemon' />
       </div>
-    
+      <div className="card-footer">
+        <span className="card-footer-title">{name}</span>
+      </div>
+    </div>
+
   );
 }
 
