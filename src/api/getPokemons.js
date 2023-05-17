ea2d477051cbef419301db4b9122a85bffe76f5f
Axel-github <ouskornikov@mail.ru>
2023-05-17 11:24:14 +0300

    Сделал кэширование запросов

--- src/api/getPokemons.js
+++ src/api/getPokemons.js
@@ -1,6 +1,15 @@
 import axios from "axios";
 import getPokemonSprite from "../helpers/getPokemonSprite";
 export async function getPokemons(limit, offset) {
+  const cacheKey = `pokemons-${limit}-${offset}`;
+  const cache = await caches.open('pokeapi-cache');
+  const cachedResponse = await cache.match(cacheKey);
+
+  if (cachedResponse) {
+    const pokemons = await cachedResponse.json();
+    return pokemons;
+  }
+
   let data = JSON.stringify({
     "query": "query samplePokeAPIquery($limit: Int, $offset: Int) {\n  results: pokemon_v2_pokemon(limit: $limit, offset: $offset) {\n    name\n    id\n    weight\n  }\n}\n",
     "variables": {
@@ -19,7 +28,6 @@ export async function getPokemons(limit, offset) {
       'accept': '*/*',
       'accept-language': 'en-US,en;q=0.9',
       'content-type': 'application/json',
-      // 'cookie': '_ga=GA1.2.1566717068.1683195340; __gads=ID=dd305e65c690553e-22b27bbcbadd000c:T=1683195340:RT=1683195340:S=ALNI_MZBoH9m29hQ-4AdTmVVqB5sDmgneA; __gpi=UID=00000bf53906960d:T=1683195340:RT=1684225247:S=ALNI_MY-9MYhiMdED09a80JAVBJcOEYCVQ; _gid=GA1.2.315050861.1684225248',
       'origin': 'https://beta.pokeapi.co',
       'referer': 'https://beta.pokeapi.co/graphql/console/',
       'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
@@ -39,10 +47,14 @@ export async function getPokemons(limit, offset) {
   const pokemons = response.data.data.results;
 
   for (let i = 0; i < pokemons.length; i++) {
-    // pokemons[i].spriteUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
     pokemons[i].spriteUrl = getPokemonSprite(pokemons[i].id, 'animated');
   }
+  
+  const cacheResponse = new Response(JSON.stringify(pokemons));
+  await cache.put(cacheKey, cacheResponse.clone());
 
   return pokemons;
-
 }
+
+
+
